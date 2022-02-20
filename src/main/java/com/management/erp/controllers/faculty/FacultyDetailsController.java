6ac721d24664ca4469189d978ec08f63f535037f
Vasu Dev Sall <vasudevsall@gmail.com>
2022-02-21 01:11:57 +0530

    Find student details

--- src/main/java/com/management/erp/controllers/faculty/FacultyDetailsController.java
+++ src/main/java/com/management/erp/controllers/faculty/FacultyDetailsController.java
@@ -2,12 +2,15 @@ package com.management.erp.controllers.faculty;
 
 import com.management.erp.models.repository.CourseModel;
 import com.management.erp.models.repository.FacultyModel;
+import com.management.erp.models.repository.StudentModel;
 import com.management.erp.repositories.CourseRepository;
 import com.management.erp.services.FindFacultyService;
+import com.management.erp.services.FindStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
@@ -23,6 +26,9 @@ public class FacultyDetailsController {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private FindStudentService findStudentService;
+
     @RequestMapping(value = "/details", method = RequestMethod.GET)
     public @ResponseBody FacultyModel getFacultyDetails(Principal principal) {
         return findFacultyService.findFacultyModelByEmail(principal.getName());
@@ -32,5 +38,10 @@ public class FacultyDetailsController {
     public @ResponseBody List<CourseModel> findAllCourses(Principal principal) {
         FacultyModel facultyModel = findFacultyService.findFacultyModelByEmail(principal.getName());
         return courseRepository.findAllByFaculty(facultyModel);
+    }
+
+    @RequestMapping(value = "/student", method = RequestMethod.GET)
+    public @ResponseBody StudentModel findStudentDetails(@RequestParam String entry) {
+        return findStudentService.findStudentModelById(entry);
     }
 }
